42fb611f511e74c93803dae967124bd6ab639932
Alcamech <alcamech@gmail.com>
2021-09-19 20:45:30 -0400

    basic project setup for java discord bot using JPA

---
+++ src/main/java/com/mr_skeltal/Main.java
@@ -0,0 +1,14 @@
+package com.mr_skeltal;
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static void main(String[] args) throws LoginException {
+        JDA jdaBuilder = JDABuilder.createDefault("token").build();
+    }
+}
